b0d19b8ddca2f1e1c2bcc43bb9a25b8929030148
Nwobodo Ikenna <nikenna06@gmail.com>
2024-02-20 11:27:16 +0100

    Update project page with new CSS styles and add dynamic background colors for each project.

--- src/data/projectlist.js
+++ src/data/projectlist.js
@@ -6,7 +6,7 @@ const projects = [
     stack: ["React", "Tailwind CSS", "Firebase"],
     link: "https://schedulex.vercel.app/",
     bg: "ferr",
-    image: require('../assets/images/sx.png'),
+    image: require("../assets/images/sx.png"),
     git: "https://github.com/ikenna-nwobodo/schedulex",
   },
   {
@@ -17,7 +17,7 @@ const projects = [
     link: "https://rundwn-one.vercel.app/",
     git: "https://github.com/ikenna-nwobodo/rundwn",
     bg: "home",
-    image: require('../assets/images/rn.png'),
+    image: require("../assets/images/rn.png"),
   },
   {
     title: "Mintlify",
@@ -25,7 +25,7 @@ const projects = [
     stack: ["React", "Tailwind CSS"],
     link: "https://mintlify-zeta.vercel.app/",
     bg: "def",
-    image: require('../assets/images/mt.png'),
+    image: require("../assets/images/mt.png"),
     git: "https://github.com/ikenna-nwobodo/mintlify",
   },
   {
@@ -34,6 +34,8 @@ const projects = [
     stack: ["React", "Tailwind CSS"],
     link: "https://quickcash-theta.vercel.app/",
     git: "https://github.com/ikenna-nwobodo/schedulex",
+    image: require("../assets/images/qc.png"),
+    bg: "qc",
   },
   {
     title: "Dictionary",
@@ -41,6 +43,8 @@ const projects = [
       "A dictionary application that enables users to look up words, explore their different meanings, and listen to their various pronunciations.",
     stack: ["HTML", "CSS", "Javascript"],
     link: "https://i-dictionary.vercel.app/",
+    image: require("../assets/images/dt.png"),
+    bg: "def",
   },
   {
     title: "GetLinked",
@@ -48,6 +52,8 @@ const projects = [
       "A website developed within a tight deadline of less than 5 hours for a competition.",
     stack: ["React", "CSS"],
     link: "https://get-linked-five.vercel.app/",
+    image: require("../assets/images/gl.png"),
+    bg: "gl",
   },
 ];
 
--- src/views/ProjectPage.jsx
+++ src/views/ProjectPage.jsx
@@ -1,9 +1,113 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import projects from "../data/projectlist";
 
 function ProjectPage() {
+  const [show, setShow] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    if (window.scrollY > lastScrollY) {
+      // if scroll down hide the navbar
+      setShow(true);
+    } else {
+      // if scroll up show the navbar
+      setShow(false);
+    }
+    // remember current page location to use in the next move
+    setLastScrollY(window.scrollY);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    // cleanup function
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [lastScrollY]);
+
   return (
-    <div className="heading text-white bg-green-700 w-[85%] min-h-screen mt-28">
-      ProjectPage
+    <div className="text-white w-[85%] overflow-hidden h-screen">
+      <div className="relative mt-28">
+        <p className="heading md:text-[6rem] mb-4 text-6xl w-full tracking-tighter capitalize font-medium">
+          Projects
+        </p>
+        <div className="bg-gradient-to-b from-transparent to-black w-full h-[15%] absolute z-40 bottom-0"></div>
+        <div className="relative">
+          <div
+            className={`bg-gradient-to-t from-transparent to-black w-full h-[10%] absolute z-40 top-0 ${
+              show && "hidden"
+            }`}
+          ></div>
+          <div className="relative top-0 h-[75vh] overflow-auto">
+            <div className="projects flex justify-center items-center mb-[10vh] h-fit flex-col gap-8 md:gap-20">
+              {projects.map((project) => {
+                return (
+                  <div
+                    className={`project project-1 flex flex-col md:flex-row gap-4 md:gap-8 w-full relative ${
+                      projects.lastIndexOf(project) + 1 === 1 && "z-50"
+                    }`}
+                  >
+                    <p
+                      className={`text-[4rem] md:text-[8rem] heading leading-tight `}
+                    >
+                      {projects.lastIndexOf(project) + 1}.
+                    </p>
+                    <div className="md:flex w-full justify-between items-center md:gap-14">
+                      <div className="flex-1 shadow-xl flex justify-center items-center h-[25vh] md:h-[40vh] rounded-2xl md:shadow-sm bg-transparent relative overflow-hidden">
+                        <div
+                          className={`absolute ${project.bg} h-full w-full`}
+                        ></div>
+                        <img
+                          src={project.image}
+                          alt=""
+                          className="md:absolute z-30 md:left-40 rounded-2xl shadow-2xl w-[270px] md:w-[500px]"
+                        />
+                      </div>
+                      <div className="relative mt-4 h-5/6 md:mt-0 w-full md:w-5/12 flex flex-col gap-2 md:gap-6">
+                        <p className="text-3xl md:text-[4rem] font-medium tracking-tighter head">
+                          {project.title}
+                        </p>
+                        <div className="text-xs flex gap-4 text-black md:mt-2">
+                          {project.stack.map((tech) => {
+                            return (
+                              <span className="bg-white px-2 py-0.5 rounded-full">
+                                {tech}
+                              </span>
+                            );
+                          })}
+                        </div>
+                        <p className="text-sm tracking-wide md:mt-0 mt-3 text-center md:text-left">
+                          {project.description}
+                        </p>
+                        <div className="md:absolute bottom-0 mt-6 md:mt-0 flex justify-center md:justify-start gap-6 items-center">
+                          <a
+                            href={project.link}
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            className="bg-white text-black text-sm font-medium px-6 py-2 md:py-3 gap-2 rounded-lg flex items-center md:hover:-translate-y-1"
+                          >
+                            Live Preview
+                            <span class="material-symbols-rounded text-[14px]">
+                              open_in_new
+                            </span>
+                          </a>
+                          <a
+                            href={project.git}
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            className="md:hover:-translate-y-1"
+                          >
+                            <i class="fa-brands fa-github text-2xl md:text-3xl"></i>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
